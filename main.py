a372bea67c6e61a745aa6196f391f9eaf3979816
Alberto Lacerda <albertotads@outlook.com>
2026-09-01 18:59:38 -0300

    adicionado os outros metodos

--- main.py
+++ main.py
@@ -5,14 +5,49 @@ app= FastAPI(title="API de Cursos", description="API para gestionar cursos", ver
 
 class Curso(BaseModel):
     id: int
-    nombre: str
-    descripcion: str
-    duracion: int
+    nome: str
+    descricao: str
+    duracao: int
     nivel: str
     
+cursos = []   
     
 @app.get("/")
 def home():
-    return {"message": "Bienvenido a la API de Cursos"}    
-    
-    
+    return {"message": "Bienvenido a la API de Cursos"}
+
+@app.get("/cursos")
+def exibir_cursos(): 
+    return cursos
+
+@app.post("/cursos")
+def adicionar_cursos(curso: Curso):
+       curso.id = len(cursos) + 1
+       cursos.append(curso)
+       return {"message": "Curso agregado exitosamente", "curso": curso}
+   
+@app.get("/cursos/{curso_id}")
+def obter_curso_por_id(curso_id: int):
+    for curso in cursos:
+        if curso.id == curso_id:
+            return curso
+    return {"message": "Curso no encontrado"}
+
+@app.put("/cursos/{curso_id}")
+def atualizar_curso(curso_id: int, curso_atualizado: Curso):
+    for curso in cursos:
+        if curso.id == curso_id:
+            curso.nome = curso_atualizado.nome
+            curso.descricao = curso_atualizado.descricao
+            curso.duracao = curso_atualizado.duracao
+            curso.nivel = curso_atualizado.nivel
+            return {"message": "Curso actualizado exitosamente", "curso": curso}
+    return {"message": "Curso no encontrado"}
+
+@app.delete("/cursos/{curso_id}")
+def excluir_curso(curso_id: int):
+    for curso in cursos:
+        if curso.id == curso_id:
+            cursos.remove(curso)
+            return {"message": "Curso eliminado exitosamente"}
+    return {"message": "Curso no encontrado"}  
